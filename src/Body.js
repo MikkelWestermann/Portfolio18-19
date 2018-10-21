598ee37fac866c986307fdaf8e72989b721391ef
Mikkel Westermann <mikkelwestermann@Mikkels-iMac.local>
2018-10-21 14:48:47 +0200

    Create Structure

---
+++ src/Body.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Body.css';
+
+class Body extends Component {
+  render() {
+    return(
+      <div id="Body">The Body</div>
+    );
+  }
+}
+
+export default Body;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Header from './Header';
+import Body from './Body';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<h1> Hello World! </h1>, document.getElementById('root'));
+ReactDOM.render(
+      <div className="App">
+        <Header />
+        <Body />
+      </div>
+  , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
