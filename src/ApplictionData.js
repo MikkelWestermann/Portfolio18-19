b408a83f08e867b2c3565568082dd5ce54a72a3d
Mikkel Westermann <mikkelwestermann@Mikkels-iMac.local>
2018-11-28 22:13:14 +0100

    Update Application data + Add platform links

--- src/ApplictionData.js
+++ src/ApplictionData.js
@@ -4,71 +4,103 @@ export const data = [
     name: "Alien Aircraft",
     icon: "AlienAircraft_icon.png",
     background: "AA_MilkyWay_portfolio_bg.png",
+    appURL: "https://itunes.apple.com/us/app/alien-aircraft/id1029056111?mt=8",
+    playURL: "https://play.google.com/store/apps/details?id=com.MikkelWestermann.AlienAircraft"
   },
   {
     id: 1,
     name: "Knuckleball",
     icon: "Knuckleball_icon_2.png",
     background: "Knuckleball_portfolio_bg.png",
+    appURL: "https://itunes.apple.com/us/app/knuckleball/id1269290959?mt=8",
+    playURL: "https://play.google.com/store/apps/details?id=com.MikkelWestermann.Knuckleball"
   },
   {
     id: 2,
     name: "Rocks & Rollin'",
     icon: "R&R_AppIcon1.png",
     background: "CloudyCrown_Midday_Right.png",
+    appURL: "https://itunes.apple.com/us/app/rocks-rollin/id1331604471?mt=8",
+    playURL: "https://play.google.com/store/apps/details?id=com.MikkelWestermann.RocksAndRollin"
   },
   {
     id: 3,
     name: "Radii",
     icon: "Radii_icon.png",
     background: "Radii_portfolio_bg.png",
+    appURL: "https://itunes.apple.com/us/app/radii/id1436695795?ls=1&mt=8",
+    playURL: "https://play.google.com/store/apps/details?id=com.MikkelWestermann.Radii"
   },
   {
     id: 4,
     name: "Subpixel",
     icon: "Subpixel_icon.png",
     background: "Subpixel_portfolio_bg.png",
+    appURL: "https://itunes.apple.com/us/app/subpixel/id1441796951?ls=1&mt=8",
+    playURL: "https://play.google.com/store/apps/details?id=com.MikkelWestermann.Subpixel"
   },
   {
     id: 5,
     name: "Split The Droplet",
     icon: "Split_icon.png",
-    background: "Split_portfolio_bg.png",
+    background: "SplitTheDroplet_portfolio_bg.png",
+    appURL: "https://itunes.apple.com/us/app/split-the-droplet/id1441917594?ls=1&mt=8",
+    playURL: "https://play.google.com/store/apps/details?id=com.MikkelWestermann.SplitTheDroplet"
   },
   {
     id: 6,
     name: "Planet Pentagon",
     icon: "PlanetPentagon_icon.png",
     background: "PlanetPentagon_portfolio_bg.png",
+    appURL: "https://itunes.apple.com/us/app/planet-pentagon/id1441793475?ls=1&mt=8",
+    playURL: "https://play.google.com/store/apps/details?id=com.MikkelWestermann.PlanetPentagon"
   },
   {
     id: 7,
     name: "Third Line",
     icon: "ThirdLine_icon.png",
     background: "ThirdLine_portfolio_bg.png",
+    appURL: "https://itunes.apple.com/us/app/third-line/id1441792562?ls=1&mt=8",
+    playURL: "https://play.google.com/store/apps/details?id=com.MikkelWestermann.ThirdLine"
   },
   {
     id: 8,
     name: "Kolor Bird",
     icon: "ColorBird_icon.png",
     background: "KolorBird_portfolio_bg.png",
+    appURL: "https://itunes.apple.com/us/app/kolor-bird/id1441794902?mt=8",
+    playURL: "https://play.google.com/store/apps/details?id=com.MikkelWestermann.KolorBird"
   },
   {
     id: 9,
     name: "Slide to Side",
     icon: "SlideToSide_icon.png",
     background: "SlideToSide_portfolio_bg.png",
+    appURL: "https://itunes.apple.com/us/app/slide-to-side/id1441793975?ls=1&mt=8",
+    playURL: "https://play.google.com/store/apps/details?id=com.MikkelWestermann.SlideToSide"
   },
   {
     id: 10,
     name: "Proper Path",
     icon: "Path_icon.png",
-    background: "Path_portfolio_bg.png",
+    background: "ProperPath_portfolio_bg.png",
+    appURL: "https://itunes.apple.com/us/app/proper-path/id1441917529?ls=1&mt=8",
+    playURL: "https://play.google.com/store/apps/details?id=com.MikkelWestermann.ProperPath"
   },
   {
     id: 11,
     name: "Suber Dash",
     icon: "SuperDash_icon.png",
-    background: "SuberDash_portfolio_bg.png",
+    background: "SuperDash_portfolio_bg.png",
+    appURL: "https://itunes.apple.com/us/app/suber-dash/id1441796436?ls=1&mt=8",
+    playURL: "https://play.google.com/store/apps/details?id=com.MikkelWestermann.SuberDash"
+  },
+  {
+    id: 12,
+    name: "Hyper Highway",
+    icon: "HyperHighway_icon.png",
+    background: "HyperHighway_portfolio_bg.png",
+    appURL: "https://itunes.apple.com/us/app/hyper-highway/id1443425636?ls=1&mt=8",
+    playURL: "https://play.google.com/store/apps/details?id=com.MikkelWestermann.HyperHighway"
   }
 ]
--- src/Card.js
+++ src/Card.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import './Card.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { fab } from '@fortawesome/free-brands-svg-icons'
 
-const Card = ({ id, name, icon, background, shade }) => {
+import appstore from './appstore.svg';
+import googleplay from './googleplay.png';
+
+const Card = ({ id, name, icon, background, appURL, playURL }) => {
   var divStyle = {
     backgroundImage: `url(https://andiscle.sirv.com/Portfolio/${id}/${background}?w=1500&h=1500)`
   }
   return(
       <div className="Card" style={divStyle}>
-        <div className="forground">
-          <img alt='icon' src={`https://andiscle.sirv.com/Portfolio/${id}/${icon}?w=200&h=200`} width="200" height="200" />
-          <div className="download">
-            <a href="./"><FontAwesomeIcon size="4x" icon={['fab', 'google-play']}/></a>
-            <a href="./"><FontAwesomeIcon size="4x" icon={['fab', 'app-store']}/></a>
-          </div>
+        <img alt='icon' src={`https://andiscle.sirv.com/Portfolio/${id}/${icon}?w=200&h=200`} width="200" height="200" className='icon'/>
+        <div className="download">
+          <a href={ appURL }><img src={ appstore } alt='app store' className='storeLinkApple'/></a>
+          <a href={ playURL }><img src={ googleplay } alt='google play' className='storeLinkGoogle'/></a>
         </div>
       </div>
   );
