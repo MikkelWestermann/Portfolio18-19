072b3be659ff388ae8d8cbb1555f5bf7eb3c2110
Mikkel Westermann <mikkelwestermann@Mikkels-iMac.local>
2018-12-07 16:11:13 +0100

    Add favicon + links to websites

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,7 +5,7 @@ class Header extends Component {
   render() {
     return (
       <div id="header">
-        <img src="https://andiscle.sirv.com/Portfolio/Logo/Logo_SlideToSide.png?w=175" width="175" height="121" alt="logo" />
+        <img src="https://andiscle.sirv.com/Portfolio/Logo/Logo_SlideToSide.png?w=200" width="200" height="103" alt="logo" />
       </div>
     );
   }
