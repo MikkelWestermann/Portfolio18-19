8b15a83873aa459f34fc79b7ff384a08cb901727
Mikkel Westermann <mikkelwestermann@Mikkels-iMac.local>
2018-12-07 17:28:12 +0100

    Updates

---
+++ precache-manifest.134135d1ed3a3e039f092a41632e613d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "93f9d7171830488b8dd5",
+    "url": "/Portfolio/static/css/main.8a180abd.chunk.css"
+  },
+  {
+    "revision": "93f9d7171830488b8dd5",
+    "url": "/Portfolio/static/js/main.93f9d717.chunk.js"
+  },
+  {
+    "revision": "bd17625cbc1bc3947161",
+    "url": "/Portfolio/static/js/1.bd17625c.chunk.js"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "2928664fe1fc6aca88583a6f606d60ba",
+    "url": "/Portfolio/static/media/appstore.2928664f.svg"
+  },
+  {
+    "revision": "db9b21a1c41f3dcd9731e1e7acfdbb57",
+    "url": "/Portfolio/static/media/googleplay.db9b21a1.png"
+  },
+  {
+    "revision": "86411025c46f4c47192795aeff694e61",
+    "url": "/Portfolio/index.html"
+  }
+];
